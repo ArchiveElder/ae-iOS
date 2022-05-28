c9ee9910bb652cc2e3995adcb4b401d96651dc43
eilyri <khaeun1029@gmail.com>
2022-05-28 21:27:38 +0900

    initial commit

---
+++ aeVeganDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
