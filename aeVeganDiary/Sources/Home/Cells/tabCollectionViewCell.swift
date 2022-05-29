04f605d2c6f24d53d2ef037df7093f0b9a4fd4c4
eilyri <khaeun1029@gmail.com>
2022-05-30 00:20:47 +0900

    [Feat] 식사 탭 CollectionView 생성

---
+++ aeVeganDiary/Sources/Home/Cells/tabCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  tabCollectionViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/05/30.
+//
+
+import UIKit
+
+class tabCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tabBackgroundView: UIView!
+    @IBOutlet weak var mealLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- aeVeganDiary/Sources/Home/HomeViewController.swift
+++ aeVeganDiary/Sources/Home/HomeViewController.swift
@@ -8,15 +8,18 @@
 import UIKit
 import FSCalendar
 
-class HomeViewController: BaseViewController {
+class HomeViewController: BaseViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
     @IBOutlet weak var weekCalendarView: FSCalendar!
+    
+    @IBOutlet weak var tabCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setNavigationTitle(title: "기록")
         
+        //FSCalendar Custom
         weekCalendarView.delegate = self
         weekCalendarView.dataSource = self
         weekCalendarView.scope = .week
@@ -24,10 +27,29 @@ class HomeViewController: BaseViewController {
         weekCalendarView.headerHeight = 3
         weekCalendarView.appearance.headerMinimumDissolvedAlpha = 0.0
         weekCalendarView.appearance.weekdayTextColor = .darkGray
+        
+        //CollectionView
+        tabCollectionView.delegate = self
+        tabCollectionView.dataSource = self
+        tabCollectionView.register(UINib(nibName: "tabCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "tabCollectionViewCell")
     }
 
 }
 
-extension HomeViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let mealList = ["아침", "점심", "저녁"]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "tabCollectionViewCell", for: indexPath) as! tabCollectionViewCell
+        cell.mealLabel.text = mealList[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: tabCollectionView.frame.width / 3, height: tabCollectionView.frame.height)
+    }
     
 }
