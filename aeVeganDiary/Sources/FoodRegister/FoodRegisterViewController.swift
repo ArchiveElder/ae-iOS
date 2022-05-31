a13dce9a36f4cb6b64c3016fd0d7aa5a0cd61881
eilyri <khaeun1029@gmail.com>
2022-05-31 17:18:54 +0900

    [Feat] 식단 상세 테이블뷰, 셀 생성

--- aeVeganDiary/Sources/FoodRegister/FoodRegisterViewController.swift
+++ aeVeganDiary/Sources/FoodRegister/FoodRegisterViewController.swift
@@ -11,15 +11,33 @@ class FoodRegisterViewController: BaseViewController {
     var foodImage = UIImage()
 
     @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var detailTableView: UITableView!
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNavigationTitle(title: "식사 등록하기")
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+        detailTableView.register(UINib(nibName: "DetailTableViewCell", bundle: nil), forCellReuseIdentifier: "DetailTableViewCell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         foodImageView.image = foodImage
+        
     }
+}
+
+extension FoodRegisterViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailTableViewCell", for: indexPath) as! DetailTableViewCell
+        return cell
+    }
+    
 }
