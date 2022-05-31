d6dd7668ceaec2805a2dfebef05135928e6a8dca
eilyri <khaeun1029@gmail.com>
2022-05-31 16:11:04 +0900

    [Feat] 식사 등록하기 화면 기초 완성

--- aeVeganDiary/Configuration/Base/BaseViewController.swift
+++ aeVeganDiary/Configuration/Base/BaseViewController.swift
@@ -21,7 +21,7 @@ class BaseViewController: UIViewController {
             UINavigationBar.appearance().scrollEdgeAppearance = appearance
         }
         
-        view.backgroundColor = .gray4
+        //view.backgroundColor = .gray4
         navigationController?.navigationBar.isTranslucent = false
         
         //navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
