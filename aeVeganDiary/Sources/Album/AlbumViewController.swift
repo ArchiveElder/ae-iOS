9e0691c74fc78efdc942e390ba10d228f1f8cf42
eilyri <khaeun1029@gmail.com>
2022-06-01 17:06:16 +0900

    [Feat] 식사 앨범 페이지 collectionViewCell 완료

--- aeVeganDiary/Sources/Album/AlbumViewController.swift
+++ aeVeganDiary/Sources/Album/AlbumViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class AlbumViewController: UIViewController {
+class AlbumViewController: BaseViewController {
 
+    @IBOutlet weak var albumCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setNavigationTitle(title: "식사 앨범")
+        
+        albumCollectionView.delegate = self
+        albumCollectionView.dataSource = self
+        albumCollectionView.register(UINib(nibName: "AlbumCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "AlbumCollectionViewCell")
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AlbumViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCollectionViewCell", for: indexPath) as! AlbumCollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 12
+        return CGSize(width: width, height: width)
+    }
 }
---
+++ aeVeganDiary/Sources/Album/Cells/AlbumCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumCollectionViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/06/01.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
