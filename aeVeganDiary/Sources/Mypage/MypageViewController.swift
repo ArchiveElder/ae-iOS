64ad0542ede31bebaebbc7c817b66e2e20cf45ea
안소정 <80646857+ahneve@users.noreply.github.com>
2022-06-03 20:33:10 +0900

    [ Feat ] 마이페이지 구현

--- aeVeganDiary/Sources/Mypage/MypageViewController.swift
+++ aeVeganDiary/Sources/Mypage/MypageViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class MypageViewController: UIViewController {
 
+    @IBOutlet var profile_img: UIImageView!
+    @IBOutlet var badge_img: UIImageView!
+    @IBOutlet var earth_img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        profile_img.image = UIImage(named: "img1.jpeg")
+        badge_img.image = UIImage(named: "tree.png")
+        earth_img.image = UIImage(named: "earth.png")
         // Do any additional setup after loading the view.
+        
+        profile_img.layer.cornerRadius = profile_img.frame.height/2
+        profile_img.layer.borderWidth = 1
+        profile_img.layer.borderColor = UIColor.clear.cgColor
+        profile_img.clipsToBounds=true
+        
+        badge_img.layer.cornerRadius = badge_img.frame.height/2
+        badge_img.layer.borderWidth = 1
+        badge_img.layer.borderColor = UIColor.clear.cgColor
+        badge_img.clipsToBounds=true
+        
     }
 
 
