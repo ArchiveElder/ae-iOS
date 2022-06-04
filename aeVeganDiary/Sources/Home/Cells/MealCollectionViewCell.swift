af8ca8519d7616cf3f7bf61cb8510e6c71fca7b1
eilyri <khaeun1029@gmail.com>
2022-06-04 16:39:46 +0900

    [Feat] 식사 CollectionView에 들어갈 식사 목록 cell 추가

---
+++ aeVeganDiary/Sources/Home/Cells/MealCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MealCollectionViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/06/04.
+//
+
+import UIKit
+
+class MealCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var mealTableView: UITableView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
