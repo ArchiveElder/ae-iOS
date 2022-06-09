df03a344c8c3f7a689697650cc3151e3984c1847
안소정 <80646857+ahneve@users.noreply.github.com>
2022-06-09 20:22:48 +0900

    [Feat] 온보딩 구현

---
+++ aeVeganDiary/Sources/OnBoarding/BodyInitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BodyInitViewController.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/06/09.
+//
+
+import UIKit
+
+class BodyInitViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setBackButton()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ aeVeganDiary/Sources/OnBoarding/NicknameInitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NicknameInitViewController.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/06/09.
+//
+
+import UIKit
+
+class NicknameInitViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+ 
+
+}
