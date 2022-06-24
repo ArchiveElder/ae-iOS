96cc17aa9e7bd29bfcbca820a1ab9b4d82cf8ca4
eilyri <khaeun1029@gmail.com>
2022-06-24 22:33:30 +0900

    [Feat] 영양소 자세히 보기 화면 생성

---
+++ aeVeganDiary/Sources/NutrientDetail/NutrientDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NutrientDetailViewController.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/06/24.
+//
+
+import UIKit
+
+class NutrientDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
