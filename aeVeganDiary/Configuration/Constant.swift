5be5c665022e74d1727cd5f69b6fcd0bcdf7c7cd
eilyri <khaeun1029@gmail.com>
2022-06-30 22:56:55 +0900

    [Feat] JWT Header 변수 추가

--- aeVeganDiary/Configuration/Constant.swift
+++ aeVeganDiary/Configuration/Constant.swift
@@ -10,5 +10,5 @@ import Alamofire
 struct Constant {
     static let BASE_URL = "http://localhost:8080"
     
-    //static var HEADERS: HTTPHeaders = ["Authorization": "Bearer \(UserDefaults.standard.string(forKey: "UserJwt") ?? "")"]
+    static var HEADERS: HTTPHeaders = ["Authorization": "Bearer \(UserDefaults.standard.string(forKey: "UserJwt") ?? "")"]
 }
