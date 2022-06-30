bbe40a0a756f5f3717e2199aa71b9e2b9b0451de
eilyri <khaeun1029@gmail.com>
2022-06-30 15:42:55 +0900

    [Feat] 식단 상세 정보 페이지 서버 통신 데이터 보여주는 작업

--- aeVeganDiary/Sources/FoodDetail/MealDetailViewController.swift
+++ aeVeganDiary/Sources/FoodDetail/MealDetailViewController.swift
@@ -9,34 +9,46 @@ import UIKit
 
 class MealDetailViewController: BaseViewController {
 
-    @IBOutlet var kcalView: UIView!
-    @IBOutlet var nutrView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var calLabel: UILabel!
+    @IBOutlet weak var carbLabel: UILabel!
+    @IBOutlet weak var proLabel: UILabel!
+    @IBOutlet weak var fatLabel: UILabel!
+    @IBOutlet weak var foodImageView: UIImageView!
     
     var record_id: Int?
+    let meals = ["아침", "점심", "저녁"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        setBackButton()
+        setDismissButton()
         setNavigationTitle(title: "상세 정보")
-        
-        kcalView.layer.cornerRadius = 8
-        nutrView.layer.cornerRadius = 8
-        kcalView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMinXMinYCorner)
-        nutrView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMinXMinYCorner)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         showIndicator()
-        
+        let input = MealDetailInput(record_id: record_id)
+        MealDetailDataManager().requestData(input, viewController: self)
     }
 }
 
 extension MealDetailViewController {
     func getData(result: MealDetailResponse) {
         dismissIndicator()
+        let data = result.data[0]
+        dateLabel.text = "\(data.date) \(data.time)"
+        textLabel.text = data.text
+        calLabel.text = data.cal
+        carbLabel.text = data.carb
+        proLabel.text = data.protein
+        fatLabel.text = data.fat
+        let url = URL(string: data.image_url)
+        foodImageView.load(url: url!)
     }
     
     func failedToRequest(message: String) {
