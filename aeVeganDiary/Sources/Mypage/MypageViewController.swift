5a16ad71bb9479914ee9664121483b6834ab6df8
안소정 <80646857+ahneve@users.noreply.github.com>
2022-06-30 15:44:17 +0900

    [Feat] 마이페이지 수정

--- aeVeganDiary/Sources/Mypage/MypageViewController.swift
+++ aeVeganDiary/Sources/Mypage/MypageViewController.swift
@@ -8,40 +8,26 @@
 import UIKit
 
 class MypageViewController: UIViewController {
-
-    @IBOutlet var profile_img: UIImageView!
-    @IBOutlet var badge_img: UIImageView!
-    @IBOutlet var earth_img: UIImageView!
+//BaseViewController로 바꾸기
+    
+    @IBOutlet var myInfoButton: UIButton!
+    @IBOutlet var profileImg: UIImageView!
+    @IBAction func moveInfo(_ sender: Any) {
+        navigationController?.pushViewController(MyInfoViewController(), animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        profile_img.image = UIImage(named: "img1.jpeg")
-        badge_img.image = UIImage(named: "tree.png")
-        earth_img.image = UIImage(named: "earth.png")
-        // Do any additional setup after loading the view.
+        //setNavigationTitle(title: "마이페이지")
         
-        profile_img.layer.cornerRadius = profile_img.frame.height/2
-        profile_img.layer.borderWidth = 1
-        profile_img.layer.borderColor = UIColor.clear.cgColor
-        profile_img.clipsToBounds=true
-        
-        badge_img.layer.cornerRadius = badge_img.frame.height/2
-        badge_img.layer.borderWidth = 1
-        badge_img.layer.borderColor = UIColor.clear.cgColor
-        badge_img.clipsToBounds=true
+        profileImg.layer.cornerRadius = profileImg.frame.height/2
+        profileImg.layer.borderWidth = 1
+        profileImg.layer.borderColor = UIColor.clear.cgColor
+        profileImg.clipsToBounds=true
         
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
