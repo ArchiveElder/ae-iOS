bba815e3c69fc00938bf0a2dc47c50643bbfbcd0
eilyri <khaeun1029@gmail.com>
2022-07-03 23:50:32 +0900

    [Fix] 반복 작성된 함수 삭제

--- aeVeganDiary/Sources/Mypage/DataManager/MyInfoDataManager2.swift
+++ aeVeganDiary/Sources/Mypage/DataManager/MyInfoDataManager2.swift
@@ -8,30 +8,13 @@
 import Alamofire
 
 class MyInfoDataManager2{
-    
-    func getMyInfoData(viewController: MyInfoViewController){
-        AF.request("\(Constant.BASE_URL)/api/userinfo", method: .get, encoding: JSONEncoding.default, headers: Constant.HEADERS)
-            .validate()
-            .responseDecodable(of: MyInfoResponse.self) {
-                response in
-                switch response.result{
-                case .success(let response):
-                    viewController.getData(result: response)
-                    print("성공")
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-    }
-    
     func updateMyInfo(_ parameters: MyInfoInput, viewController: MyInfoViewController){
         AF.request("\(Constant.BASE_URL)/api/userupdate", method: .put, parameters: parameters, encoder: JSONParameterEncoder.default, headers: Constant.HEADERS)
             .validate()
             .responseData(emptyResponseCodes: [200], completionHandler: { response in
                 switch response.result {
                 case .success( _):
-                    //viewController.getData()
-                    print(response)
+                    viewController.update()
                 case .failure(let error):
                     print(error.localizedDescription)
                 }
