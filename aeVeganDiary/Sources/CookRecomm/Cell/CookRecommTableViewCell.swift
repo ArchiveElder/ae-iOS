3986f189cd6a6417362d68cc46b71a98601dfe85
안소정 <80646857+ahneve@users.noreply.github.com>
2022-07-12 17:23:46 +0900

    [Feat] 요리 추천 검색

---
+++ aeVeganDiary/Sources/CookRecomm/Cell/CookRecommTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CookRecommTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/07/11.
+//
+
+import UIKit
+
+class CookRecommTableViewCell: UITableViewCell {
+
+    @IBOutlet var cookRecommtvclb: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- aeVeganDiary/Sources/CookRecomm/CookRecommViewController.swift
+++ aeVeganDiary/Sources/CookRecomm/CookRecommViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class CookRecommViewController: UIViewController {
-
+class CookRecommViewController: BaseViewController, UITableViewDelegate {
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
+    
+    
+    var shownFoods = [String]()
+    let allFoods = ["비빔밥", "비비빅","비비빅1", "볶음밥"]
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setNavigationTitle(title: "검색하기")
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "CookRecommTableViewCell", bundle: nil), forCellReuseIdentifier: "CookRecommTableViewCell")
+        setBackButton()
+        setup()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setup() {
+        tableView.dataSource = self
+        searchBar
+            .rx.text
+            .orEmpty
+            .debounce(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .filter{ !$0.isEmpty }
+            .subscribe(onNext: { [unowned self] query in
+                self.shownFoods = self.allFoods.filter { $0.hasPrefix(query) }
+                self.tableView.reloadData()
+    })
+            //.addDisposableTo(disposeBag)
     }
-    */
-
 }
+
+
+extension CookRecommViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownFoods.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CookRecommTableViewCell", for: indexPath) as! CookRecommTableViewCell
+        cell.cookRecommtvclb?.text = shownFoods[indexPath.row]
+        
+        return cell
+    }
+}
+
