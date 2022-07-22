5e8e6783508546fec730aae1ea3dc6c63b40c590
eilyri <khaeun1029@gmail.com>
2022-07-22 22:23:50 +0900

    [Feat] Base URL 변경, http 통신 설정

--- aeVeganDiary/Configuration/Constant.swift
+++ aeVeganDiary/Configuration/Constant.swift
@@ -8,7 +8,7 @@
 import Alamofire
 
 struct Constant {
-    static let BASE_URL = "http://localhost:8080"
+    static let BASE_URL = "http://ec2-15-165-139-29.ap-northeast-2.compute.amazonaws.com:8080"
     
     static var HEADERS: HTTPHeaders = ["Authorization": "Bearer \(UserDefaults.standard.string(forKey: "UserJwt") ?? "")"]
 }
