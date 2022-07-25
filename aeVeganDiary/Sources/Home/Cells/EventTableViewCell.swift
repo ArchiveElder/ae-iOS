ee77cdf15c9af0f428b7772720bc55a9562d9265
eilyri <khaeun1029@gmail.com>
2022-07-25 14:58:50 +0900

    [Feat] 일정 table view 생성

---
+++ aeVeganDiary/Sources/Home/Cells/EventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/07/23.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
