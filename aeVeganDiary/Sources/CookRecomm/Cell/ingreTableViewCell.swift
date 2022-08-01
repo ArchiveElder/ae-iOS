35034e50e4f326bd17576a84b93013c89417fb5d
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-01 15:49:33 +0900

    [Feat] 테이블뷰 테스트

---
+++ aeVeganDiary/Sources/CookRecomm/Cell/ingreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ingreTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/08/01.
+//
+
+import UIKit
+
+class ingreTableViewCell: UITableViewCell {
+
+    @IBOutlet var ingreLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
