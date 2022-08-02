0da12624d77e773a9f685fa407e3e29a324aea3a
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-03 07:15:00 +0900

    [Feat] RecommInner CustomView 구현

---
+++ aeVeganDiary/Sources/CookRecomm/Cell/hasTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  hasTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/08/02.
+//
+
+import UIKit
+
+class hasTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
