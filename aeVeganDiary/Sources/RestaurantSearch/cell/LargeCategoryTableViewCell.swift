b7a5b93d762f6e450ecb2037b782eadab830c161
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-10 17:16:12 +0900

    [Feat] LargeCategory UI

---
+++ aeVeganDiary/Sources/RestaurantSearch/cell/LargeCategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LargeCategoryTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/08/10.
+//
+
+import UIKit
+
+class LargeCategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var LargeCategoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
