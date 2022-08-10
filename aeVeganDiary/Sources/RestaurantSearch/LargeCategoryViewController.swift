d165d69ba26bae86dd5df15e780c1167681fa802
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-10 17:28:52 +0900

    [Chore] View 변경, tableView UI

---
+++ aeVeganDiary/Sources/RestaurantSearch/LargeCategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LargeCategoryViewController.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/08/10.
+//
+
+import UIKit
+
+class LargeCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ aeVeganDiary/Sources/RestaurantSearch/cell/MiddleCategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MiddleCategoryTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/08/10.
+//
+
+import UIKit
+
+class MiddleCategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var MiddleCategoryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
