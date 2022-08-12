b329d00be7b2b98888f70171d76457b92c2c7919
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-12 15:14:05 +0900

    [Feat] 지역 reselect

--- aeVeganDiary/Sources/RestaurantSearch/LargeCategoryViewController.swift
+++ aeVeganDiary/Sources/RestaurantSearch/LargeCategoryViewController.swift
@@ -12,17 +12,23 @@ import RxSwift
 
 class LargeCategoryViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet var regionBackButton: UIButton!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var chooseLable: UILabel!
     
     var regionResponse: RegionResponse?
     var regionArray = [""]
-    var tableState = 1
+    var tableState : Int = 1
     
-    let largeCity = ["서울특별시", "경기도", "인천광역시", "강원도", "경상남도", "경상북도", "광주광역시","대구광역시", "대전광역시", "부산광역시", "세종특별자치시"," 울산광역시", "전라남도", "전라북도", "제주특별자치도", "충청남도", "충청북도"]
+    @IBAction func reselctButton(_ sender: Any) {
+        tableState = 1
+        tableView.reloadData()
+    }
+    
+    let largeCity = ["서울특별시", "경기도", "인천광역시", "강원도", "경상남도", "경상북도", "광주광역시","대구광역시", "대전광역시", "부산광역시", "세종특별자치시","울산광역시", "전라남도", "전라북도", "제주특별자치도", "충청남도", "충청북도"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        regionBackButton.isHidden = true
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: "LargeCategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "LargeCategoryTableViewCell")
@@ -47,6 +53,7 @@ extension LargeCategoryViewController : UITableViewDataSource {
         
         
         if tableState == 1 {
+            chooseLable.text = "지역을 선택하세요"
             let cell = tableView.dequeueReusableCell(withIdentifier: "LargeCategoryTableViewCell", for: indexPath)
             cell.textLabel?.text = largeCity[indexPath.row]
             return cell
@@ -73,6 +80,7 @@ extension LargeCategoryViewController : UITableViewDataSource {
         print(regionArray)
         tableState = 2
         chooseLable.text = "시를 선택하세요"
+        regionBackButton.isHidden = false
         tableView.reloadData()
     }
     
