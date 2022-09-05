bd88da3de6720986db77cd017a92cf0a044dd41f
eilyri <khaeun1029@gmail.com>
2022-09-05 11:50:13 +0900

    [Feat] 애플로그인 설정

--- aeVeganDiary/Sources/Splash/Login/LoginViewController.swift
+++ aeVeganDiary/Sources/Splash/Login/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KakaoSDKUser
+import AuthenticationServices
 
 class LoginViewController: BaseViewController {
 
@@ -39,6 +40,18 @@ class LoginViewController: BaseViewController {
                 }
             }
         }
+    }
+    
+    @IBOutlet weak var appleLoginButton: ASAuthorizationAppleIDButton!
+    @IBAction func appleLoginButtonAction(_ sender: Any) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
     }
     
     override func viewDidLoad() {
@@ -78,5 +91,45 @@ extension LoginViewController {
     func failedToRequest(message: String) {
         dismissIndicator()
         presentAlert(message: message)
+    }
+}
+
+// MARK: - Apple Login Extensions
+
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    // 로그인 진행하는 화면 표출
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    // Apple ID 연동 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+            // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+            
+            if let authorizationCode = appleIDCredential.authorizationCode,
+               let identityToken = appleIDCredential.identityToken,
+               let tokenString = String(data: identityToken, encoding: .utf8) {
+                print("tokenString: \(tokenString)")
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    // Apple ID 연동 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("authorization didcmpletewitherror")
     }
 }
