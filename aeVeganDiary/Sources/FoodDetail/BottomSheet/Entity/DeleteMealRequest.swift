447791cd45fe100817aea869e6ba8c3e453d890c
안소정 <80646857+ahneve@users.noreply.github.com>
2022-10-24 13:31:38 +0900

    [Refactor] API 1-6

---
+++ aeVeganDiary/Sources/FoodDetail/BottomSheet/Entity/DeleteMealRequest.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteMealRequest.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/10/24.
+//
+
+import Foundation
+
+struct DeleteMealRequest: Encodable {
+    var recordId : Int
+}
---
+++ aeVeganDiary/Sources/FoodDetail/BottomSheet/Entity/DeleteMealResponse.swift
@@ -0,0 +1,15 @@
+//
+//  DeleteMealResponse.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/10/24.
+//
+
+import Foundation
+
+struct DeleteMealResponse: Decodable {
+    var isSuccess: Bool
+    var code: Int
+    var message: String
+    var result: String
+}
