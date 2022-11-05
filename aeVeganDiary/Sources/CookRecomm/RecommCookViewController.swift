c4b5d044bd779a5c1216a0e90f63cca9ae0f193b
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-06 00:45:46 +0900

    [Feat] 냉장고 재료 삭제

--- aeVeganDiary/Sources/CookRecomm/RecommCookViewController.swift
+++ aeVeganDiary/Sources/CookRecomm/RecommCookViewController.swift
@@ -12,6 +12,7 @@ import SafariServices
 
 class RecommCookViewController: BaseViewController, UISearchBarDelegate, UIWebViewDelegate, RecommCollectionViewCellDelegate {
     
+    lazy var selectedBox: [Int:Int] = [:]
     lazy var ingreDataManager: IngreDataManagerDelegate = IngreDataManager()
     
     @IBOutlet var ingreImageView: UIImageView!
@@ -49,6 +50,7 @@ class RecommCookViewController: BaseViewController, UISearchBarDelegate, UIWebVi
     var cookRecomm = [CookRecomm?]()
     var searchBarFocused = false
     var ingreArr = [String]()
+    var recommIngreArr = [String]()
     var disposeBag = DisposeBag()
     
     var selected : Int? = 0
@@ -66,6 +68,8 @@ class RecommCookViewController: BaseViewController, UISearchBarDelegate, UIWebVi
         setNavigationTitle(title: "채식 요리 추천")
         setResetButton()
         
+        ingreArr = UserDefaults.standard.array(forKey: "UserIngre") as! [String]
+        
         //Ingre Search
         searchBar.delegate = self
         searchTableView.isHidden = true
@@ -78,7 +82,6 @@ class RecommCookViewController: BaseViewController, UISearchBarDelegate, UIWebVi
         ingreTableView.dataSource = self
         ingreTableView.delegate = self
         ingreTableView.register(UINib(nibName: "IngreTableViewCell", bundle: nil), forCellReuseIdentifier: "IngreTableViewCell")
-        
         
         //collectionView
         recommCollectionView.delegate = self
@@ -195,7 +198,6 @@ class RecommCookViewController: BaseViewController, UISearchBarDelegate, UIWebVi
                         .sorted { ($0.hasPrefix(query) ? 0 : 1) < ($1.hasPrefix(query) ? 0 : 1)}
                     self.searchTableView.reloadData()
                 })
-            
         }
     }
     
@@ -230,23 +232,54 @@ extension RecommCookViewController : IngreViewDelegate{
     }}
     
     
-    extension RecommCookViewController : UITableViewDataSource, UITableViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+extension RecommCookViewController : UITableViewDataSource, UITableViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, IngreTableviewCellDelegate{
+    func didTapIngreDeleteButton(with ingreLabel: String) {
+        if(ingreArr.firstIndex(of: ingreLabel) != nil){
+            if let index = ingreArr.firstIndex(of: ingreLabel) {
+                ingreArr.remove(at: index)
+                if(recommIngreArr.firstIndex(of: ingreLabel) != nil){
+                    if let index = recommIngreArr.firstIndex(of: ingreLabel) {
+                        recommIngreArr.remove(at: index)
+                    }
+                }
+                print(recommIngreArr)
+            }
+            UserDefaults.standard.set(ingreArr, forKey: "UserIngre")
+            ingreTableView.reloadData()
+        }
+    }
+    
+    
+    func didTapIngreCheckButton(with ingreLabel: String, with ingreSelected: Bool) {
+        if(ingreSelected == true){
+            if(recommIngreArr.firstIndex(of: ingreLabel) == nil){
+                recommIngreArr.append(ingreLabel)
+            }
+        } else {
+            if(recommIngreArr.firstIndex(of: ingreLabel) != nil){
+                if let index = recommIngreArr.firstIndex(of: ingreLabel) {
+                    recommIngreArr.remove(at: index)
+                }
+            }
+        }
+        if(recommIngreArr.count != 0){
+            recommButton.isHidden = false
+        } else {
+            recommButton.isHidden = true
+        }
+        var ingreInput = IngreInput(ingredients: recommIngreArr)
+        CookRecommDataManager().postRcommCook(ingreInput, viewController: self)
         
-        /*
-         func getIngreData(result: IngreResponse){
-         dismissIndicator()
-         self.ingreResponse = result
-         self.ingre = result.data
-         }
-         */
-        func getRecomm(result: CookRecommResponse){
+    }
+    
+    func getRecomm(result: CookRecommResponse){
             dismissIndicator()
             self.cookRecommResponse = result
             self.cookRecomm = result.foodDto
         }
         
-        // collectionView 크기 설정
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    // collectionView 크기 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
             if collectionView == tabCollectionView {
                 return CGSize(width: tabCollectionView.frame.width / 3 - 1.2, height: tabCollectionView.frame.height)
             } else {
@@ -254,11 +287,11 @@ extension RecommCookViewController : IngreViewDelegate{
             }
         }
         
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
             return 3
         }
         
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             if collectionView == tabCollectionView {
                 if(cookRecomm.count != 0 && cookRecomm[indexPath.row]!.food.count != 0){
                     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommTabCollectionViewCell", for: indexPath) as! RecommTabCollectionViewCell
@@ -323,14 +356,14 @@ extension RecommCookViewController : IngreViewDelegate{
             
         }
         
-        @objc func toSafari() {
+    @objc func toSafari() {
             let url = URL(string: cookRecomm[selected ?? 0]?.recipeUrl ?? "")!
             let safariView : SFSafariViewController = SFSafariViewController(url: url)
             present(safariView, animated: true, completion: nil)
         }
         
-        // 탭 collectionView의 cell들 누르면 실행되는 코드
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // 탭 collectionView의 cell들 누르면 실행되는 코드
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
             if collectionView == tabCollectionView {
                 selected = indexPath.row
                 recommCollectionView.scrollToItem(at: NSIndexPath(item: selected!, section: 0) as IndexPath, at: .right, animated: false)
@@ -340,7 +373,7 @@ extension RecommCookViewController : IngreViewDelegate{
         }
         
         
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             var count : Int = 0
             if tableView == searchTableView {
                 count = shownFoods.count
@@ -355,18 +388,34 @@ extension RecommCookViewController : IngreViewDelegate{
                     recommButton.isHidden = false
                     ingreImageView.isHidden = true
                     ingreLabel.isHidden = true
-                } else {
+                    if(recommIngreArr.count==0){
+                        recommButton.isHidden = true
+                        ingreImageView.isHidden = true
+                        ingreLabel.isHidden = true
+                        recommCollectionView.isHidden = true
+                        tabCollectionView.isHidden = true
+                    } else if(recommIngreArr.count != 0){
+                        ingreImageView.isHidden = true
+                        ingreLabel.isHidden = true
+                        recommButton.isHidden = false
+                        recommCollectionView.isHidden = true
+                        tabCollectionView.isHidden = true
+                    }
+                }
+                //냉장고에 재료 없을 때
+                else {
                     recommButton.isHidden = true
                     ingreImageView.isHidden = false
                     ingreLabel.isHidden = false
                     recommCollectionView.isHidden = true
                     tabCollectionView.isHidden = true
                 }
+                
             }
             return count
         }
         
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             if tableView == searchTableView {
                 let cell = searchTableView.dequeueReusableCell(withIdentifier: "CookRecommTableViewCell", for: indexPath)
                 cell.textLabel?.text = shownFoods[indexPath.row]
@@ -375,25 +424,16 @@ extension RecommCookViewController : IngreViewDelegate{
                 let cell = ingreTableView.dequeueReusableCell(withIdentifier: "IngreTableViewCell", for: indexPath) as! IngreTableViewCell
                 cell.ingreLabel?.text = ingreArr[indexPath.row]
                 cell.textLabel?.font = UIFont.systemFont(ofSize: 15)
+                cell.selectionStyle = .none
+
+                cell.delegate = self
                 return cell
             }
             
         }
-        
-        func tableView (_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
-            if tableView == searchTableView {
-                
-            } else {
-                ingreTableView.beginUpdates()
-                ingreArr.remove(at: indexPath.row)
-                ingreTableView.deleteRows(at: [indexPath], with: .fade)
-                ingreTableView.endUpdates()
-                var ingreInput = IngreInput(ingredients: ingreArr)
-                CookRecommDataManager().postRcommCook(ingreInput, viewController: self)
-            }
-        }
-        
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             if tableView == searchTableView {
                 //음식 선택 시
                 searchBar.text = ""
@@ -401,17 +441,18 @@ extension RecommCookViewController : IngreViewDelegate{
                 print(currentCell)
                 if(ingreArr.firstIndex(of: currentCell!) == nil) {
                     ingreArr.append(currentCell ?? "")
+                    UserDefaults.standard.set(ingreArr, forKey: "UserIngre")
                 } else {
                     presentAlert(message: "재료가 이미 있습니다.")
                 }
-                var ingreInput = IngreInput(ingredients: ingreArr)
-                CookRecommDataManager().postRcommCook(ingreInput, viewController: self)
+                //var ingreInput = IngreInput(ingredients: ingreArr)
+                //CookRecommDataManager().postRcommCook(ingreInput, viewController: self)
                 ingreTableView.reloadData()
                 tableView.isHidden = true
                 dismissKeyboard()
                 searchBar.setShowsCancelButton(false, animated: true)
             } else if tableView == ingreTableView {
-                
+
             }
         }
         
