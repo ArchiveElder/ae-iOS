3fcfec61952d42eb747e344e9fd8f55017544d86
eilyri <khaeun1029@gmail.com>
2022-11-06 22:03:02 +0900

    [Feat] 마이페이지 홈화면으로 이동, 게시판 페이지 생성

--- aeVeganDiary/Sources/Board/BoardViewController.swift
+++ aeVeganDiary/Sources/Board/BoardViewController.swift
@@ -7,23 +7,31 @@
 
 import UIKit
 
-class BoardViewController: UIViewController {
+class BoardViewController: BaseViewController {
 
+    @IBOutlet weak var boardTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setNavigationTitle(title: "커뮤니티")
+        view.backgroundColor = .white
+        
+        boardTableView.delegate = self
+        boardTableView.dataSource = self
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BoardViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    
 }
---
+++ aeVeganDiary/Sources/Board/Cell/BoardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BoardTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 권하은 on 2022/11/06.
+//
+
+import UIKit
+
+class BoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
