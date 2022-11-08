586864426c09a771c90926c036044ee49b0b0653
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-08 13:49:14 +0900

    [Feat] 내가 쓴 글, 스크랩 한 글 페이지로 넘어가기

---
+++ aeVeganDiary/Sources/Mypage/Scrap/ScrapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScrapViewController.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/11/08.
+//
+
+import UIKit
+
+class ScrapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
