9f1ae3f3d8b50121f849ed999674910552258468
eilyri <khaeun1029@gmail.com>
2022-11-11 21:33:43 +0900

    [Feat] 게시글 cell 생성

--- aeVeganDiary/Sources/Board/Cell/BoardTableViewCell.swift
+++ aeVeganDiary/Sources/Board/Cell/BoardTableViewCell.swift
@@ -6,18 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class BoardTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var isPhotoImageView: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    var disposeBag = DisposeBag()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // cell reuse 시에 초기화되도록 작업하기
+        //avatarImage.image = nil
+        disposeBag = DisposeBag()
     }
     
+    func updateUI(post: Post) {
+        categoryLabel.text = post.groupName
+        nicknameLabel.text = post.nickname
+        titleLabel.text = post.title
+        bookmarkButton.isSelected = post.isScraped == 1 ? true : false
+    }
 }
