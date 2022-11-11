bfd42ab35e64eaf79fda26947dca593cd701c5fc
eilyri <khaeun1029@gmail.com>
2022-11-11 21:34:24 +0900

    [Feat] 게시글 화면
    - mvvm, RxSwift 사용

--- aeVeganDiary/Sources/Board/BoardViewController.swift
+++ aeVeganDiary/Sources/Board/BoardViewController.swift
@@ -6,32 +6,61 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class BoardViewController: BaseViewController {
 
     @IBOutlet weak var boardTableView: UITableView!
+    
+    let viewModel = BoardViewModel.shared
+    let disposeBag = DisposeBag()
+    let cellId = "BoardTableViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setNavigationTitle(title: "커뮤니티")
         view.backgroundColor = .white
+        //boardTableView.register(UINib(nibName: "BoardTableViewCell", bundle: nil), forCellReuseIdentifier: "BoardTableViewCell")
         
-        boardTableView.delegate = self
-        boardTableView.dataSource = self
+        setBinding()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viewModel.reloadData()
+    }
+    
+    
+    private func setBinding() {
+        //boardTableView.rx.setDelegate(self).disposed(by: bag)
+        // tableView datasource -- 데이터 개수, 셀에 들어갈 내용
+        viewModel.posts
+            .observe(on: MainScheduler.instance)
+            .filter { !$0.isEmpty }
+            .bind(to: boardTableView.rx.items(cellIdentifier: cellId, cellType: BoardTableViewCell.self)) { index, item, cell in
+                cell.updateUI(post: item)
+            }
+            .disposed(by: disposeBag)
+        
+        // tableView delegate
+        // tableView.rx.itemSelected -> indexPath
+        boardTableView.rx.modelSelected(Post.self)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] post in
+                self?.presentDetail(of: post)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func presentDetail(of post: Post) {
+        /*guard let detailVC = storyboard?.instantiateViewController(withIdentifier: "MemberDetailViewController") as? MemberDetailViewController else {
+            return
+        }
+        viewModel.selectedMember = member
+        present(detailVC, animated: true, completion: nil)*/
     }
 
-}
-
-extension BoardViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        return cell
-    }
-    
-    
 }
