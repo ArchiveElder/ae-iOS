29ce4c69448527b15fdc6c628a398a800d6d2986
eilyri <khaeun1029@gmail.com>
2022-11-13 11:59:12 +0900

    [Feat] TextField, TextView 로직 추가

--- aeVeganDiary/Sources/Board/Post/PostViewController.swift
+++ aeVeganDiary/Sources/Board/Post/PostViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PostViewController: BaseViewController {
     
+    lazy var postDataManager: PostDataManagerDelegate = PostDataManager()
+    
     @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
@@ -22,15 +24,31 @@ class PostViewController: BaseViewController {
         dismissKeyboard()
     }
     
+    let doneButton: UIButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
         dismissButton()
+        setDoneButton()
         keyboardDismissButton.isHidden = true
+        setNavigationTitle(title: "글쓰기")
+        doneButton.isEnabled = false
         
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        titleTextField.addTarget(self, action: #selector(editingStatusChanged), for: .editingChanged)
+        categoryTextField.addTarget(self, action: #selector(editingStatusChanged), for: .editingChanged)
+    }
+    
+    @objc func editingStatusChanged() {
+        if isPostingAvailable() {
+            doneButton.isEnabled = true
+        } else {
+            doneButton.isEnabled = false
+        }
     }
 
     // 뒤로가기/중간이탈 버튼 추가
@@ -49,6 +67,43 @@ class PostViewController: BaseViewController {
         presentAlert(title: "글쓰기를 취소하시겠어요?", message: "작성한 내용은 저장되지 않습니다.", isCancelActionIncluded: true, preferredStyle: .alert, handler: {_ in
             self.navigationController?.popToRootViewController(animated: true)
         })
+    }
+    
+    func setDoneButton() {
+        let normalTitle = NSAttributedString(string: "완료", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .semibold), NSAttributedString.Key.foregroundColor: UIColor.darkGreen])
+        let disableTitle = NSAttributedString(string: "완료", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .semibold), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        doneButton.setAttributedTitle(normalTitle, for: .normal)
+        doneButton.setAttributedTitle(disableTitle, for: .disabled)
+        doneButton.addTarget(self, action: #selector(donePost), for: .touchUpInside)
+        doneButton.frame = CGRect(x: 0, y: 0, width: 26, height: 20)
+        let addDoneButton = UIBarButtonItem(customView: doneButton)
+        
+        self.navigationItem.setRightBarButton(addDoneButton, animated: false)
+    }
+    
+    @objc func donePost() {
+        presentAlert(title: "글쓰기를 완료하시겠어요?", message: nil, isCancelActionIncluded: true, preferredStyle: .alert, handler: {_ in
+            self.postContent()
+        })
+    }
+    
+    func postContent() {
+        showIndicator()
+        let title = titleTextField.text ?? ""
+        let content = contentTextView.text ?? ""
+        let groupName = categoryTextField.text ?? ""
+        let postingInput = PostRequest(title: title, content: content, groupName: groupName)
+        let userId = UserDefaults.standard.integer(forKey: "UserId")
+        
+        postDataManager.postPosting(postingInput, multipartFileList: nil, userIdx: userId, delegate: self)
+    }
+    
+    func isPostingAvailable() -> Bool {
+        if titleTextField.text != "" && contentTextView.text != "" && categoryTextField.text != "" {
+            return true
+        }
+        return false
     }
     
     @objc func handleKeyboardNotification(notification: NSNotification) {
@@ -74,4 +129,25 @@ class PostViewController: BaseViewController {
     deinit {
         NotificationCenter().removeObserver(self)
     }
+}
+
+extension PostViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        editingStatusChanged()
+        return true
+    }
+}
+
+extension PostViewController: PostViewDelegate {
+    func didSuccessPost(_ result: PostResponse) {
+        dismissIndicator()
+        self.dismiss(animated: true)
+    }
+    
+    func failedToPost(message: String, code: Int) {
+        dismissIndicator()
+        presentAlert(message: message)
+    }
+    
+    
 }
