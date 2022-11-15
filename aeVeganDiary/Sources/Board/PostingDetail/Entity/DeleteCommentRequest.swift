f012a5d640df75a14f5be1691992ddda5f75050e
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-15 14:47:48 +0900

    [Feat] 댓글 삭제 Entity

---
+++ aeVeganDiary/Sources/Board/PostingDetail/Entity/DeleteCommentRequest.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteCommentRequest.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/11/15.
+//
+
+import Foundation
+
+struct DeleteCommentRequest: Encodable {
+    var commentIdx: Int
+}
