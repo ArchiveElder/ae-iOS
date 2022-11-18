738add338514ea118aecd3b39029da9802c05b04
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-18 19:25:32 +0900

    [Feat] Scrap TableViewCell UI

---
+++ aeVeganDiary/Sources/Mypage/Scrap/New Group/MyScrapTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MyScrapTableViewCell.swift
+//  aeVeganDiary
+//
+//  Created by 소정의 Mac on 2022/11/18.
+//
+
+import UIKit
+
+class MyScrapTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var isPhotoImageView: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
