76f0283fdf6be905898ca9aceff99b723d47bc99
안소정 <80646857+ahneve@users.noreply.github.com>
2022-11-18 18:36:03 +0900

    [Feat] API 31-6 연결

--- aeVeganDiary/Sources/Mypage/Posting/PostingDataManager.swift
+++ aeVeganDiary/Sources/Mypage/Posting/PostingDataManager.swift
@@ -7,4 +7,28 @@
 
 
 import Foundation
+import Alamofire
+import RxSwift
 
+
+class PostingDataManager {
+    static func getPostings(userIdx : Int, page: Int) -> Observable<[PostingLists]> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer \(UserManager.shared.jwt)"]
+        return Observable.create { observer -> Disposable in
+            AF.request("http://15.164.40.10:8080/posting/mypost/\(userIdx)?page=\(page)&sort=idx,desc", method: .get, headers: headers)
+                .responseDecodable(of: PostingResponse.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        print(data)
+                        if response.response?.statusCode == 200 {
+                            observer.onNext(data.postsLists ?? [])
+                        }
+                    case .failure(let error):
+                        observer.onError(error)
+                        print(error.localizedDescription)
+                    }
+                }
+            return Disposables.create()
+        }
+    }
+}
